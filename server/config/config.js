6f39a987019e506e81ef530e61dfe6a673ee54b1
Rodrigo <rodrigo.martinez@webexport.com.ar>
2022-01-30 16:13:17 -0300

    Revert "Edit dependencie morgan"
    This reverts commit 30834cdf58639809e9ec101f4411518845647e4f.

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 const DB = process.env.DB;
+console.log(DB)
 const MONGO = require("mongoose");
 const CON = MONGO.connection;
 const JWT_KEY = "rodrizio@343";
