30834cdf58639809e9ec101f4411518845647e4f
Rodrigo <rodrigo.martinez@webexport.com.ar>
2022-01-30 14:18:24 -0300

    Edit dependencie morgan

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 const DB = process.env.DB;
-console.log(DB)
 const MONGO = require("mongoose");
 const CON = MONGO.connection;
 const JWT_KEY = "rodrizio@343";
