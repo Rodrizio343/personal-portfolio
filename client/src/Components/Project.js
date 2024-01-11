01483a96cb6e866aaa5d0f42a786dd8713f46369
Rodrigo <rodrigo.martinez@webexport.com.ar>
2024-01-11 22:10:33 +0000

    Projects box sixe fix

--- client/src/Components/Project.js
+++ client/src/Components/Project.js
@@ -26,8 +26,8 @@ export default function Project({
 
   return (
     <div
-      className="bg-white shadow h-100 w-100"
-      style={{ borderRadius: "10px", maxHeight: "370px", overflowY: "auto" }}
+      className="bg-white shadow w-100"
+      style={{ borderRadius: "10px", height: "350px", overflowY: "auto" }}
     >
       <img
         src={image || ImagePlaceholder}
@@ -35,63 +35,67 @@ export default function Project({
         className="img-fluid w-100"
         style={{
           objectFit: "cover",
-          maxHeight: "200px",
+          maxHeight: "130px",
           borderTopLeftRadius: "10px",
           borderTopRightRadius: "10px",
         }}
       />
-      <div className="px-4 py-3">
-        <h6>{title}</h6>
-        <p style={{ fontSize: "12px" }} className="text-muted">
-          {description}
-        </p>
-
-        <div className="d-flex flex-wrap">
-          {technologies.map((tech, i) => (
-            <Badge variant="secondary" className="mr-1 mb-1" key={i} pill>
-              <small className="font-weight-light text-white">{tech}</small>
-            </Badge>
-          ))}
+      <div className="px-4 py-3 d-flex flex-column justify-content-between" style={{ height: "220px"}}>
+        <div>
+          <h6>{title}</h6>
+          <p style={{ fontSize: "12px" }} className="text-muted">
+            {description}
+          </p>
         </div>
-
-        <div className="d-flex my-1">
-          {github && (
-            <a
-              href={github}
-              style={styles}
-              className="text-muted mr-2"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Repositorio
-            </a>
-          )}
-
-          {link && (
-            <a
-              href={link}
-              style={styles}
-              className="text-muted"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Demo
-            </a>
-          )}
-        </div>
-
-        {user._id && (
-          <div className="d-flex">
-            <Button variant="link" size="sm" onClick={toggleDeleteModal}>
-              <span className="text-danger mr-1">Eliminar</span>
-              <BiTrash style={{ fill: "#dc3545" }} />
-            </Button>
-            <Button variant="link" size="sm" onClick={toggleUpdateModal}>
-              <span className="text-primary mr-1">Editar</span>
-              <BiEditAlt style={{ fill: "#007bff" }} />
-            </Button>
+        
+        <div>
+          <div className="d-flex flex-wrap">
+            {technologies.map((tech, i) => (
+              <Badge variant="secondary" className="mr-1 mb-1" key={i} pill>
+                <small className="font-weight-light text-white">{tech}</small>
+              </Badge>
+            ))}
           </div>
-        )}
+
+          <div className="d-flex my-1">
+            {github && (
+              <a
+                href={github}
+                style={styles}
+                className="text-muted mr-2"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Repositorio
+              </a>
+            )}
+
+            {link && (
+              <a
+                href={link}
+                style={styles}
+                className="text-muted"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Demo
+              </a>
+            )}
+          </div>
+
+          {user._id && (
+            <div className="d-flex">
+              <Button variant="link" size="sm" onClick={toggleDeleteModal}>
+                <span className="text-danger mr-1">Eliminar</span>
+                <BiTrash style={{ fill: "#dc3545" }} />
+              </Button>
+              <Button variant="link" size="sm" onClick={toggleUpdateModal}>
+                <span className="text-primary mr-1">Editar</span>
+                <BiEditAlt style={{ fill: "#007bff" }} />
+              </Button>
+            </div>
+          )}
+        </div>
       </div>
 
       <DeleteProjectModal
